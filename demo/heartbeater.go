2a4d03a6d4f0f25b5c2469f6046de89932af3c06
Dane Mortensen <mortensendane@gmail.com>
2018-11-05 02:01:55 -0800

    master and a worker that beats every second

--- demo/heartbeater.go
+++ demo/heartbeater.go
@@ -1,9 +1,49 @@
-package heartbeater
+package main
 
 import (
+   "flag"
+   "fmt"
+   "log"
+   "net"
+   "net/http"
+   "os"
+
    "github.com/danemortensen/heartbeat"
+   "github.com/gorilla/mux"
 )
 
+var (
+   masterAddr string
+)
+
+func checkError(err error) {
+   if err != nil {
+      fmt.Println("Error: ", err)
+      os.Exit(1)
+   }
+}
+
+func init() {
+   flag.StringVar(&masterAddr, "master", "",
+                  "This heartbeater's master (if any)")
+}
+
+
 func main() {
-   HelloWorld()
+   flag.Parse()
+   listener, err := net.Listen("tcp", ":0")
+   checkError(err)
+   router := mux.NewRouter()
+
+   if masterAddr == "" {      // I am the master node
+      me := heartbeat.Master{}
+      fmt.Println("New master at", listener.Addr())
+      router.HandleFunc("/", me.AddHeartbeater).Methods("GET")
+   } else {                  // I am a worker node
+      me := heartbeat.Worker{MasterAddr: masterAddr}
+      fmt.Println("New worker at", listener.Addr())
+      go me.BeatLoop()
+   }
+
+   log.Fatal(http.Serve(listener, router))
 }
--- heartbeat.go
+++ heartbeat.go
@@ -2,20 +2,45 @@ package heartbeat
 
 import (
    "fmt"
-)
-
-type Mode int
-const (
-   Create Mode = iota
-   Connect Mode = iota
+   "net"
+   "net/http"
+   "os"
+   "time"
 )
 
 type Heartbeater struct {
-   mode Mode
+   BeatDelay      time.Time      // delay between heartbeats
+   TableDelay     time.Time      // delay between sending table to neighbors
+   Neighbors      []string       // ip strings of neighbors I'm responsible for
 }
 
-var Three int
+type Master struct {
+   Heartbeater
+   Members        []string       // ip strings of heartbeaters
+}
 
-func HelloWorld() {
-   fmt.Println("Hello, world!")
+type Worker struct {
+   Heartbeater
+   MasterAddr     string         // ip string of master
+}
+
+func (me *Master) AddHeartbeater(w http.ResponseWriter, r *http.Request) {
+   ip, port, err := net.SplitHostPort(r.RemoteAddr)
+   checkError(err)
+   fmt.Println("New heartbeater at", ip, "on port", port)
+}
+
+func (me *Worker) BeatLoop() {
+   for range time.Tick(time.Second) {
+      resp, err := http.Get(me.MasterAddr)
+      checkError(err)
+      defer resp.Body.Close()
+   }
+}
+
+func checkError(err error) {
+   if err != nil {
+      fmt.Println("Error: ", err)
+      os.Exit(1)
+   }
 }
